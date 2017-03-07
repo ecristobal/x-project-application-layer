4ac952377e9622f31e17813e646b04b555b5f1a7
esteban.a.rodriguez <esteban.a.rodriguez@accenture.com>
2017-03-07 17:48:08 +0100

    Modified Eureka client annotation

--- application-layer/authentication-manager/src/main/java/com/x/project/application/layer/authentication/manager/AuthenticationManagerApplication.java
+++ application-layer/authentication-manager/src/main/java/com/x/project/application/layer/authentication/manager/AuthenticationManagerApplication.java
@@ -2,7 +2,7 @@ package com.x.project.application.layer.authentication.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ImportResource;
  * @author Esteban Cristóbal
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @ImportResource("classpath:/META-INF/spring/authentication-manager*.xml")
 public class AuthenticationManagerApplication {
 
