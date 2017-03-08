f4e87def2622dbd3cd67f375571e66e617b4a120
esteban.a.rodriguez <esteban.a.rodriguez@accenture.com>
2017-03-08 13:25:28 +0100

    Added request ID and parameter fields for preactions and postactions contexts

--- application-layer/postactions/postactions-api/src/main/java/com/x/project/application/layer/postactions/api/PostactionContext.java
+++ application-layer/postactions/postactions-api/src/main/java/com/x/project/application/layer/postactions/api/PostactionContext.java
@@ -1,10 +1,42 @@
 package com.x.project.application.layer.postactions.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class that stores the context for the postactions.
  * 
  * @author Esteban Cristóbal
  */
 public class PostactionContext {
+
+    private String requestId;
+
+    private Map<String, Object> outputParameters;
+
+    /**
+     * Class constructor.
+     * 
+     * @param requestId
+     *            request ID associated with this context
+     */
+    public PostactionContext(final String requestId) {
+        this.outputParameters = new HashMap<String, Object>();
+        this.requestId = requestId;
+    }
+
+    /**
+     * @return request ID
+     */
+    public String getRequestId() {
+        return requestId;
+    }
+
+    /**
+     * @return output parameter {@link Map}
+     */
+    public Map<String, Object> getOutputParameters() {
+        return outputParameters;
+    }
 
 }
--- application-layer/preactions/preactions-api/src/main/java/com/x/project/application/layer/preactions/api/PreactionContext.java
+++ application-layer/preactions/preactions-api/src/main/java/com/x/project/application/layer/preactions/api/PreactionContext.java
@@ -1,10 +1,42 @@
 package com.x.project.application.layer.preactions.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class that stores the context for the preactions.
  * 
  * @author Esteban Cristóbal
  */
 public class PreactionContext {
+
+    private String requestId;
+
+    private Map<String, Object> inputParameters;
+
+    /**
+     * Class constructor.
+     * 
+     * @param requestId
+     *            request ID associated with this context
+     */
+    public PreactionContext(final String requestId) {
+        this.inputParameters = new HashMap<String, Object>();
+        this.requestId = requestId;
+    }
+
+    /**
+     * @return request ID
+     */
+    public String getRequestId() {
+        return requestId;
+    }
+
+    /**
+     * @return input parameter {@link Map}
+     */
+    public Map<String, Object> getInputParameters() {
+        return inputParameters;
+    }
 
 }
