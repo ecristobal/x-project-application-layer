07c65b0502aeee8d3e79502a7c76f8e722816d7c
esteban.a.rodriguez <esteban.a.rodriguez@accenture.com>
2017-03-09 12:35:18 +0100

    Updated unit tests for application-service project

--- application-layer/application-service/src/test/java/com/x/project/application/layer/application/service/mock/MockBusinessDelegate.java
+++ application-layer/application-service/src/test/java/com/x/project/application/layer/application/service/mock/MockBusinessDelegate.java
@@ -3,7 +3,7 @@ package com.x.project.application.layer.application.service.mock;
 import com.x.project.application.layer.business.delegate.AbstractBusinessDelegate;
 import com.x.project.application.layer.business.delegate.exception.BusinessLogicException;
 
-public class MockServiceBusinessDelegate extends AbstractBusinessDelegate<MockApiComponent, MockApiComponent> {
+public class MockBusinessDelegate extends AbstractBusinessDelegate<MockApiComponent, MockApiComponent> {
 
     @Override
     public MockApiComponent execute(MockApiComponent inputParamters, String requestId) throws BusinessLogicException {
