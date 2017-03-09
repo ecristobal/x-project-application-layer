3426c492bf0f5af499793c12cb331e2ce20b7a81
esteban.a.rodriguez <esteban.a.rodriguez@accenture.com>
2017-03-09 10:18:01 +0100

    Removed unused import

--- application-layer/business-delegate/src/test/java/com/x/project/application/layer/business/delegate/discovery/DiscoveryServiceTest.java
+++ application-layer/business-delegate/src/test/java/com/x/project/application/layer/business/delegate/discovery/DiscoveryServiceTest.java
@@ -13,8 +13,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.test.context.ContextConfiguration;
