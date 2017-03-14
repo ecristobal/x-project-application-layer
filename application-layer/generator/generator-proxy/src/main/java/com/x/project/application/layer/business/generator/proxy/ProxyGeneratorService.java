71c40b76166a569a3b2e4585ff81208327b170b0
Esteban <ecristobalr@gmail.com>
2017-03-14 17:00:40 +0100

    Javadoc code modifications

--- application-layer/generator/generator-proxy/src/main/java/com/x/project/application/layer/business/generator/proxy/ProxyGeneratorService.java
+++ application-layer/generator/generator-proxy/src/main/java/com/x/project/application/layer/business/generator/proxy/ProxyGeneratorService.java
@@ -1,8 +1,8 @@
 package com.x.project.application.layer.business.generator.proxy;
 
 /**
- * Utility class to manage creation of JAX-WS proxies with JMS
- * transport protocol.
+ * Utility class to manage creation of JAX-WS proxies with JMS transport
+ * protocol.
  * 
  * @author Esteban Cristóbal
  */
@@ -11,10 +11,13 @@ public interface ProxyGeneratorService {
     /**
      * Creates a proxy service which routes request through a JMS queue.
      * 
+     * @param <T>
+     *            proxy interface
      * @param serviceClass
      *            service to be proxied
      * @param queueName
      *            JMS queue which transports the request
+     * 
      * @return SOAP-JMS proxy
      */
     public <T> T createJmsProxy(final Class<T> serviceClass, final String queueName);
