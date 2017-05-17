a0ee5e86f9b22fb03225ebd23c27962869cabbe1
Esteban <ecristobalr@gmail.com>
2017-05-17 22:29:05 +0200

    Removed all Spring dependencies and substituted by Spring Boot ones and refactored authentication-manager to use Spring Java configuration file.

--- application-layer/authentication-manager/src/main/java/com/x/project/application/layer/authentication/manager/AuthenticationManagerRunner.java
+++ application-layer/authentication-manager/src/main/java/com/x/project/application/layer/authentication/manager/AuthenticationManagerRunner.java
@@ -3,7 +3,6 @@ package com.x.project.application.layer.authentication.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ImportResource;
 
 /**
  * Spring boot launcher class.
@@ -12,17 +11,16 @@ import org.springframework.context.annotation.ImportResource;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@ImportResource("classpath:/META-INF/spring/authentication-manager*.xml")
 public class AuthenticationManagerRunner {
 
-	/**
-	 * Main method.
-	 * 
-	 * @param args
-	 *            program arguments
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationManagerRunner.class, args);
-	}
+    /**
+     * Main method.
+     * 
+     * @param args
+     *            program arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationManagerRunner.class, args);
+    }
 
 }
---
+++ application-layer/authentication-manager/src/main/java/com/x/project/application/layer/authentication/manager/configuration/AuthenticationManagerBeanConfiguration.java
@@ -0,0 +1,83 @@
+package com.x.project.application.layer.authentication.manager.configuration;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
+import org.apache.cxf.rs.security.oauth2.grants.clientcred.ClientCredentialsGrantHandler;
+import org.apache.cxf.rs.security.oauth2.provider.OAuthDataProvider;
+import org.apache.cxf.rs.security.oauth2.provider.OAuthJSONProvider;
+import org.apache.cxf.rs.security.oauth2.services.AccessTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.x.project.application.layer.authentication.manager.provider.JwtOauthCustomProvider;
+
+/**
+ * Spring bean configuration class.
+ * 
+ * @author Esteban Cristóbal
+ */
+@Configuration
+@EnableTransactionManagement
+@EntityScan({ "org.apache.cxf.rs.security.oauth2.common", "org.apache.cxf.rs.security.oauth2.grants",
+        "org.apache.cxf.rs.security.oauth2.tokens" })
+public class AuthenticationManagerBeanConfiguration {
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    private Bus bus;
+
+    @Bean
+    public Server restServer() throws IOException {
+        JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
+        endpoint.setBus(bus);
+        endpoint.setAddress("/oauth");
+        endpoint.setServiceBeans(Arrays.asList(this.accessTokenService()));
+        endpoint.setFeatures(Arrays.asList(new Swagger2Feature()));
+        endpoint.setProvider(new OAuthJSONProvider());
+        final Properties properties = new Properties();
+        properties.load(this.getClass().getResourceAsStream("/properties/jwt.signature.properties"));
+        properties.load(this.getClass().getResourceAsStream("/properties/jwt.encryption.properties"));
+        final Map<String, Object> propertiesMap = new HashMap<>(properties.size());
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            propertiesMap.put(entry.getKey().toString(), entry.getValue());
+        }
+        endpoint.setProperties(propertiesMap);
+        return endpoint.create();
+    }
+
+    @Bean
+    public AccessTokenService accessTokenService() {
+        final AccessTokenService accessTokenService = new AccessTokenService();
+        accessTokenService.setDataProvider(this.oauthDataProvider());
+        final ClientCredentialsGrantHandler clientCredentialsGrantHandler = new ClientCredentialsGrantHandler();
+        clientCredentialsGrantHandler.setDataProvider(this.oauthDataProvider());
+        accessTokenService.setGrantHandler(clientCredentialsGrantHandler);
+        return accessTokenService;
+    }
+
+    @Bean
+    public OAuthDataProvider oauthDataProvider() {
+        final JwtOauthCustomProvider oAuthDataProvider = new JwtOauthCustomProvider();
+        oAuthDataProvider.setAccessTokenLifetime(300L);
+        oAuthDataProvider.setRecycleRefreshTokens(true);
+        oAuthDataProvider.setUseJwtFormatForAccessTokens(true);
+        oAuthDataProvider.setEntityManagerFactory(this.entityManagerFactory);
+        return oAuthDataProvider;
+    }
+
+}
