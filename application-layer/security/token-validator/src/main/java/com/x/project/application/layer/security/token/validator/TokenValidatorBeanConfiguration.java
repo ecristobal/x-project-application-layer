d54bd6d46759a1628c1745313e8abe11c9774fd4
Esteban <ecristobalr@gmail.com>
2017-08-02 21:35:47 +0200

    Updated security configuration for services

--- application-layer/security/token-validator/src/main/java/com/x/project/application/layer/security/token/validator/TokenValidatorBeanConfiguration.java
+++ application-layer/security/token-validator/src/main/java/com/x/project/application/layer/security/token/validator/TokenValidatorBeanConfiguration.java
@@ -22,7 +22,19 @@ public class TokenValidatorBeanConfiguration extends WebSecurityConfigurerAdapte
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().and().authorizeRequests().antMatchers("/*").authenticated();
+        // @formatter:off
+        http.csrf()
+            // Enable access to Swagger definitions without being authenticated
+            .and()
+                .authorizeRequests().antMatchers("/**/swagger.*").permitAll()
+            .and()
+                .authorizeRequests().antMatchers("/api-docs?/url=/swagger.json").permitAll()
+            .and()
+                .authorizeRequests().antMatchers("/").permitAll()
+            // Restrict access only to REST services
+            .and()
+                .authorizeRequests().antMatchers("/*").authenticated();
+        // @formatter:on
     }
 
 }
